092e6639beed06a50c8b8c9152e370f4e2dd02a9
ptdtan <ptdtan@gmail.com>
2016-04-02 11:05:21 +0700

    modify rpkm

--- rpkm.py
+++ rpkm.py
@@ -16,47 +16,22 @@ class RPKMInstance(object):
     """
 
     def __init__(self, rpkm_records, IDsampleDict):
-        self._rpkm_profiles = defaultdict()
-        self._rpkm_computing(IDsampleDict, rpkm_records)
-
-    def _rpkm_computing(self, IDsampleDict, rpkm_records):
-        """compute the dict(tissue, rpkm_profile) for given rpkm record containing
-        rpkm values for multiple IDsampleDict
-
-        :IDsampleDict: list of sample objects
-        :rpkm_records: raw rpkm data read from file, dict(key=sampleID, value=rpkm)
-
-        :returns: add rpk._rpkm_profiles to self._rpkm_profiles
-        """        
-
-        def __boxplot_computing(data):
-            arr = numpy.array(data)
-            box = plt.boxplot(arr)
-            return [item.get_ydata()[1] for item in box['whishkers']]
-
-        def __inferer(gene, record):
-            arr_rpkm_stissue = defaultdict(list)
-            map(lambda x: arr_rpkm_stissue[IDsampleDict[x]._stissue].append(float(record[x])),\
-                                                              record.keys())
-            tup_rpkm_stissue = defauldict()
-            map(lambda y: setitem(tup_rpkm_stissue, y, __boxplot_computing(arr_rpkm_stissue[y])), \
-                                                                        arr_rpkm_stissue.keys())
-            self._rpkm_profiles[gene] = {stissue: values for stissue, values in tup_rpkm_stissue.items() }
-        
-        map(lambda arg : __inferer(*arg), [[gene, record] for gene, record in rpkm_records.items()])
+        pass
+        #self._rpkm_profiles = defaultdict()
+        #self._rpkm_computing(IDsampleDict, rpkm_records)
     
     @staticmethod
     def _records_from_file(filepath):
         """TODO: Reads records from file
 
         :filepath: path to RPKM file
-        :returns: dict( key=geneID, value:dict(key=sampleID, value=RPKM))
+        :returns: dict( key=geneID, value:array of expression)
 
         """
         with gzip.open(filepath, 'rb') as ofile:
             map(lambda x: ofile.readline(), range(2))
-            stissues = ofile.readline().strip().split('\t')[2:]
+            #stissues = ofile.readline().strip().split('\t')[2:]
             records = defaultdict(dict)
-            map(lambda x: setitem(records, x[0], dict(zip(stissues, x[2:]))),\
+            map(lambda x: setitem(records, x[0], x[2:]),\
                                     list(map(lambda y: y.strip().split("\t"), ofile.readlines())))
 	return records
