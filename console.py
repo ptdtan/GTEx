70b10c3b549daace99278314bed87d22f1aec22a
Duy Tan <ptdtan@gmail.com>
2016-04-09 04:13:34 +0700

    update

--- console.py
+++ console.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 import sys
 import samples as sa
-import rpkm
-import rpkm2tpm as rt
 
 samples = sa.GetSample(sys.argv[1])
 transcript_samples = open(sys.argv[2]).read().strip().split("\t")[2:]
 
-iddict = sa._IDdict(samples)
-tissuedict = sa._STISSUEdict(samples)
-#print tissuedict.keys(), tissuedict.values()
-tmp = tissuedict[transcript_samples[0]]._stissue
-print tmp, transcript_samples.index(
+IDdict = sa._IDdict(samples)
+STISSUEdict = sa._STISSUEdict(samples)
+tmp = IDdict[transcript_samples[0]]._stissue
+print "\t".join([tmp, transcript_samples.index(STISSUEdict[tmp]._id)])
 for s in transcript_samples:
-
-    #sleep(0.5)
+    if IDdict[s]._stissue != tmp:
+        print "\t".join([IDdict[s]._stissue, transcript_samples.index(s)])
