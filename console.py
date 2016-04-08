dc589ade72133a22a17472067322c2b27cc22e0c
Duy Tan <ptdtan@gmail.com>
2016-04-09 04:40:00 +0700

    update index samples

--- console.py
+++ console.py
@@ -2,14 +2,19 @@
 import sys
 import samples as sa
 
+BUFFER = sys.argv[3]
 samples = sa.GetSample(sys.argv[1])
-transcript_samples = open(sys.argv[2]).read().strip().split("\t")[4:]
+transcript_samples = open(sys.argv[2]).read().strip().split("\t")[BUFFER:]
 
 IDdict = sa._IDdict(samples)
-STISSUEdict = sa._sTISSUEdict(samples)
-tmp = IDdict[transcript_samples[0]]._stissue
-print "\t".join([tmp, "0"])
-for i, s in enumerate(transcript_samples):
-    if IDdict[s]._stissue != tmp:
-        print "\t".join([IDdict[s]._stissue, str(transcript_samples.index(s))])
-        tmp = IDdict[s]._stissue
+arr_tissues = list(map(lambda x: IDdict[x]._stissue, transcript_samples))
+tmp = arr_tissues[0]
+tmp_idx = 0
+for i, s in enumerate(arr_tissues):
+    if i == 0:
+        print "\t".join([s, str(BUFFER+i+1)])
+    elif i == len(arr_tissues) -1 :
+        print "\t".join([s, str(tmp_idx), str(BUFFER+len(arr_tissues)+1)])
+    else:
+        print "\t".join([s, str(tmp_idx), str(BUFFER+1+i)])
+        tmp_idx = i+2
