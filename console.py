11e4668cb380e2eea892b784dcd8c222b9e2e6d5
DUY TAN <ptdtan@gmail.com>
2016-04-09 04:53:09 +0700

    update

--- console.py
+++ console.py
@@ -2,20 +2,17 @@
 import sys
 import samples as sa
 
-BUFFER = str(sys.argv[3])
+BUFFER = int(sys.argv[3])
 samples = sa.GetSample(sys.argv[1])
 transcript_samples = open(sys.argv[2]).read().strip().split("\t")[BUFFER:]
 
 IDdict = sa._IDdict(samples)
 arr_tissues = list(map(lambda x: IDdict[x]._stissue, transcript_samples))
 tmp = arr_tissues[0]
-tmp_idx = 0
+samples_index = [(tmp, 0+BUFFER+1)]
 for i, s in enumerate(arr_tissues):
-    if i == 0:
-        print "\t".join([s, str(BUFFER+i+1)])
-    elif i == len(arr_tissues) -1 :
-        print "\t".join([s, str(tmp_idx), str(BUFFER+len(arr_tissues)+1)])
-    else:
-        print "\t".join([s, str(tmp_idx), str(BUFFER+1+i)])
-        tmp_idx = i+2
+    if s != tmp:
+        samples_index.append((s, i+1+BUFFER))
         tmp = s
+for i in range(len(samples_index)):
+    print samples_index[i][0], samples_index[i][1], samples_index[i+1].[1]-1
