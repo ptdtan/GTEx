d564932bcfc27d7e1f4ebb8696dcdc70dbf9373f
Son Pham <son@ace.bioinf.spbau.ru>
2016-04-09 07:25:43 +0300

    update

--- console.py
+++ console.py
@@ -3,12 +3,13 @@ import sys
 import samples as sa
 
 samples = sa.GetSample(sys.argv[1])
-transcript_samples = open(sys.argv[2]).read().strip().split("\t")[2:]
+transcript_samples = open(sys.argv[2]).read().strip().split("\t")[4:]
 
 IDdict = sa._IDdict(samples)
-STISSUEdict = sa._STISSUEdict(samples)
+STISSUEdict = sa._sTISSUEdict(samples)
 tmp = IDdict[transcript_samples[0]]._stissue
-print "\t".join([tmp, transcript_samples.index(STISSUEdict[tmp]._id)])
-for s in transcript_samples:
+print "\t".join([tmp, "0"])
+for i, s in enumerate(transcript_samples):
     if IDdict[s]._stissue != tmp:
-        print "\t".join([IDdict[s]._stissue, transcript_samples.index(s)])
+        print "\t".join([IDdict[s]._stissue, str(transcript_samples.index(s))])
+        tmp = IDdict[s]._stissue
